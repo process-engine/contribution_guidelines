2e4458ccb1e8bf23d19eb13a32bb9d3ebb6e28de
ElRaptorus <christian.werner@5minds.de>
2019-08-08 17:23:27 +0200

    :package: Add script for deleting pre-releases from github

---
+++ scripts/tools/remove_pre-release_tags.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const exec = require('child_process').exec;
+
+// All tags older than this date will be deleted.
+const cutOffDate = new Date('2019-07-16');
+
+async function getAllTags() {
+
+  const command = 'git log --tags --simplify-by-decoration --pretty="format:%ai %d"';
+  const results = await execCommand(command);
+
+  const splitResults = results.split('\n');
+
+  console.log('Filter tags with matching date');
+  const resultsWithMatchingDate = splitResults.filter((entry) => {
+    const datePart = entry.split('(')[0].trim();
+    const tagDate = new Date(datePart);
+    return cutOffDate > tagDate;
+  });
+
+  if (resultsWithMatchingDate.length === 0) {
+    console.log('No matching tags found');
+    return [];
+  }
+
+  const regExp = /v.*-pre.[^),]+/g
+  const matches = resultsWithMatchingDate.join('\n').match(regExp);
+
+  if (!matches || matches.length === 0) {
+    console.log('No matching tags found');
+    return [];
+  }
+
+  // Some tags are stored like "2018-09-20 10:28:11 +0200  (tag: v2.2.0-pre-b48, tag: v2.2.0-pre-b47)"
+  // Meaing there is more than one tag in a row.
+  const flattenedResults = [];
+  for (const tag of matches) {
+    const multipleTags = tag.indexOf(',') > -1;
+    if (multipleTags) {
+      flattenedResults.push(...tag.split(', tag: '))
+    } else {
+      flattenedResults.push(tag);
+    }
+  }
+
+  console.log(`Found ${flattenedResults.length} matching tags`);
+  for (const tag of flattenedResults) {
+    console.log(tag);
+  }
+
+  return flattenedResults;
+}
+
+async function removeTags(tagsToRemove) {
+  console.log('TAGS TO REMOVE');
+  console.log(tagsToRemove);
+
+  const concattedTags = tagsToRemove.join(' ');
+
+  console.log('Delete tags from remote solution');
+  await execCommand(`git push --delete origin ${concattedTags}`);
+
+  console.log('Delete tags locally');
+  await execCommand('git fetch --prune origin +refs/tags/*:refs/tags/*');
+}
+
+async function execCommand(command) {
+  return new Promise((resolve) => {
+    exec(command, (err, stdin, stderr) => {
+      return resolve(stdin);
+    });
+  });
+}
+
+console.log('Fetching tags...');
+execCommand('git fetch')
+  .then(() => {
+    return getAllTags();
+  })
+  .then((tagsToRemove) => {
+    if (tagsToRemove.length === 0) {
+      return;
+    }
+    return removeTags(tagsToRemove);
+  });
